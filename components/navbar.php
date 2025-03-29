91d8cd2865de3d9afd99c3979b5c6228df48c52c
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2025-03-29 13:55:45 -0400

    updated active nav link code

--- components/navbar.php
+++ components/navbar.php
@@ -3,19 +3,9 @@
   <div class="nav-inner">
     <h1>Lo bueno</h1>
     <div class="nav-links">
-      <?php if ($page === "restaurant.php"): ?>
-        <a class='active' href="restaurant.php">HOME</a>;
-        <a href="menu.php">MENU</a>
-        <a href="contact.php">CONTACT</a>
-      <?php elseif ($page === "menu.php"): ?>
-        <a href="restaurant.php">HOME</a>;
-        <a class='active' href="menu.php">MENU</a>
-        <a href="contact.php">CONTACT</a>
-      <?php elseif ($page === "contact.php"): ?>
-        <a href="restaurant.php">HOME</a>;
-        <a href="menu.php">MENU</a>
-        <a class='active' href="contact.php">CONTACT</a>
-      <?php endif; ?>
+      <a <?php if (!empty($page) && $page === 'restaurant.php'): ?> class='active' <?php endif ?> href="restaurant.php">HOME</a>;
+      <a <?php if (!empty($page) && $page === 'menu.php'): ?> class='active' <?php endif ?> href="menu.php">MENU</a>
+      <a <?php if (!empty($page) && $page === 'contact.php'): ?> class='active' <?php endif ?> href="contact.php">CONTACT</a>
     </div>
   </div>
 </nav>
